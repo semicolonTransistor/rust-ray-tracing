bc8251365d743d30eb682a629a4b0ccb9cec6bad
Jinyu Liu <liu.jinyu@psu.edu>
2024-01-02 18:02:38 -0500

    Modified testing script to do 5 runs.

--- test_script.py
+++ test_script.py
@@ -2,6 +2,8 @@ import itertools
 from pathlib import Path
 import subprocess
 
+TRIALS = 5
+
 THREADS = (
     1, 4, 8, 12, 16, 20, 24
 )
@@ -25,7 +27,7 @@ SCENE_SIZES = (
     534,
 )
 
-PREFIX = "Intel13700K-AVX-F64"
+PREFIX = "Intel13700K-AVX-F32"
 
 def main():
     test_cases = set()
@@ -48,42 +50,44 @@ def main():
     executable_path = Path("target/release/ray-tracing")
     
     for i, test_case in enumerate(test_cases):
-        print(f"Rendering scene {test_case[0]} at {test_case[1][0]}x{test_case[1][1]} using {test_case[2]} threads in {test_case[3]} mode at {test_case[4]} samples per pixel")
-        
-        test_name = f"{PREFIX}-{test_case[0]}-{test_case[1][0]}x{test_case[1][1]}-{test_case[2]}-{test_case[3]}-{test_case[4]}"
-        
-        scene_file = config_dir / f"scene-{test_case[0]}-objects.toml"
-        camera_file = config_dir / f"scene-{test_case[0]}-camera.toml"
-        
-        stat_file = output_dir / f"{test_name}.toml"
-        image_file = output_dir / f"{test_name}.png"
-        
-        std_out_file = output_dir / f"{test_name}.stdout"
-        std_err_file = output_dir / f"{test_name}.stderr"
-        
-        p = subprocess.run(
-            (
-                executable_path,
-                "--scene", scene_file,
-                "--camera", camera_file,
-                "--report", stat_file,
-                "--width", str(test_case[1][0]),
-                "--height", str(test_case[1][1]),
-                "--output-image", image_file,
-                "--render-mode", str(test_case[3]),
-                "--samples-per-pixel", str(test_case[4]),
-                "--thread-count", str(test_case[2])
-            ),
-            capture_output=True 
-        )
-        
-        with std_out_file.open("wb") as f:
-            f.write(p.stdout)
-        
-        with std_err_file.open("wb") as f:
-            f.write(p.stderr)
+        for trial in range(TRIALS):
+            print(f"Rendering scene {test_case[0]} at {test_case[1][0]}x{test_case[1][1]} using {test_case[2]} threads in {test_case[3]} mode at {test_case[4]} samples per pixel")
+            print(f"trial {trial}")
             
-        print(f"Completed scene {test_case[0]} at {test_case[1][0]}x{test_case[1][1]} using {test_case[2]} threads in {test_case[3]} mode at {test_case[4]} samples per pixel")
+            test_name = f"{PREFIX}-{test_case[0]}-{test_case[1][0]}x{test_case[1][1]}-{test_case[2]}-{test_case[3]}-{test_case[4]}-{trial}"
+            
+            scene_file = config_dir / f"scene-{test_case[0]}-objects.toml"
+            camera_file = config_dir / f"scene-{test_case[0]}-camera.toml"
+            
+            stat_file = output_dir / f"{test_name}.toml"
+            image_file = output_dir / f"{test_name}.png"
+            
+            std_out_file = output_dir / f"{test_name}.stdout"
+            std_err_file = output_dir / f"{test_name}.stderr"
+            
+            p = subprocess.run(
+                (
+                    executable_path,
+                    "--scene", scene_file,
+                    "--camera", camera_file,
+                    "--report", stat_file,
+                    "--width", str(test_case[1][0]),
+                    "--height", str(test_case[1][1]),
+                    "--output-image", image_file,
+                    "--render-mode", str(test_case[3]),
+                    "--samples-per-pixel", str(test_case[4]),
+                    "--thread-count", str(test_case[2])
+                ),
+                capture_output=True 
+            )
+            
+            with std_out_file.open("wb") as f:
+                f.write(p.stdout)
+            
+            with std_err_file.open("wb") as f:
+                f.write(p.stderr)
+                
+            print(f"Completed scene {test_case[0]} at {test_case[1][0]}x{test_case[1][1]} using {test_case[2]} threads in {test_case[3]} mode at {test_case[4]} samples per pixel")
         
 
 
